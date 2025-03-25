be36a264799bc27b4032cef3000e1c58553cb9dd
DrStonIT <107717942+DrStonIT@users.noreply.github.com>
2025-03-26 00:50:35 +0300

    Update redirect_server.py

--- redirect_server.py
+++ redirect_server.py
@@ -8,5 +8,6 @@ app = FastAPI()
 @app.get("/")
 def redirect_v2ray(key: str = Query(..., description="Encoded V2Ray Key")):
     decoded_key = urllib.parse.unquote(key)  # Декодируем ключ
-    new_url = f"v2raytun://import/{decoded_key}"  # Формируем правильную ссылку
+    new_url = f"v2raytun://import/{decoded_key}"
+    new_url = f"v2rayN://install-config?url={decoded_key}"# Формируем правильную ссылку
     return RedirectResponse(url=new_url) 
