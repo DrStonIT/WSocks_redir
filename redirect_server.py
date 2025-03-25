2aa2cb93aa230a134b8326b23479a7540f5eb33c
DrStonIT <107717942+DrStonIT@users.noreply.github.com>
2025-03-25 21:34:55 +0300

    Update redirect_server.py

--- redirect_server.py
+++ redirect_server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
+import urllib.parse
 
 app = FastAPI()
 
@@ -8,8 +9,12 @@ app = FastAPI()
 def home():
     return {"message": "Этот сервер выполняет редирект для V2Ray"}
 
-# Настраиваем редирект
-@app.get("/{path:path}")
-def redirect(path: str):
-    new_url = f"v2raytun://import/vless://945c40e3-fd50-4062-9473-39cb8d001994@de-3.wsocks.ru:443?type=tcp&security=reality&pbk=MCEDsjvqBrJGLXk-yJOsSu5-RK8fO7kkFT_RC_giNgM&fp=chrome&sni=google.com&sid=8e&spx=%2F&flow=xtls-rprx-vision#WSocks VPN Germany"
+@app.get("/{encoded_key}")
+def redirect(encoded_key: str):
+    # Декодируем ключ обратно в исходный формат
+    key = urllib.parse.unquote(encoded_key)
+
+    # Формируем корректную ссылку для V2RayTun
+    new_url = f"v2raytun://import/{key}"
+    
     return RedirectResponse(url=new_url)
