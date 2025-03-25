4569ba9f2e216674f948787574e8dae95ccc6171
DrStonIT <107717942+DrStonIT@users.noreply.github.com>
2025-03-25 20:59:07 +0300

    Update redirect_server.py

--- redirect_server.py
+++ redirect_server.py
@@ -11,5 +11,5 @@ def home():
 # Настраиваем редирект
 @app.get("/{path:path}")
 def redirect(path: str):
-    new_url = f"v2raytun://import/https://de-1.wsocks.ru:2096/SubWSocks_VPN_DE_FRA-1/{path}"
+    new_url = f"v2raytun://import/vless://945c40e3-fd50-4062-9473-39cb8d001994@de-3.wsocks.ru:443?type=tcp&security=reality&pbk=MCEDsjvqBrJGLXk-yJOsSu5-RK8fO7kkFT_RC_giNgM&fp=chrome&sni=google.com&sid=8e&spx=%2F&flow=xtls-rprx-vision#WSocks VPN Germany"
     return RedirectResponse(url=new_url)
