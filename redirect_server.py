0b11cdbcd96da1dc7dc3156cbe804d6c0fc2f884
DrStonIT <107717942+DrStonIT@users.noreply.github.com>
2025-03-25 21:57:15 +0300

    Update redirect_server.py

--- redirect_server.py
+++ redirect_server.py
@@ -4,9 +4,6 @@ import urllib.parse
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return {"message": "V2Ray Redirect Server is running!"}
 
 @app.get("/")
 def redirect_v2ray(key: str = Query(..., description="Encoded V2Ray Key")):
